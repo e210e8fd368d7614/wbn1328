5bee6d33a3bf8e75f43515db63ae77a606b3e7f7
Автор проекта "Благополучие <e210e8fd368d7614cac3337d5c5e1c3fa3c1acbc@outlook.com>
2019-10-31 04:25:56 +0300

    Add set for fork

---
+++ fork-run.js
@@ -0,0 +1,10 @@
+global.FORK_MODE = 1; //set to 1 for fork
+global.NETWORK = "WBN-MAIN"; //max 10 chars
+global.START_NETWORK_DATE = 1570903300000; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
+global.CONSENSUS_PERIOD_TIME = 2000; //ms
+global.FORK_IP_LIST = [
+  {"ip":"193.124.180.20","port":34777},
+  {"ip":"194.67.221.153","port":34777},
+  {"ip":"81.90.180.31","port":34777},
+  {"ip":"95.216.210.46","port":34777},
+  {"ip":"127.0.0.1","port":34777}];
