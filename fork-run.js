8de0133740b98c0cddad7b0933ba6ed9b650960e
e210e8fd368d7614 <e210e8fd368d7614cac3337d5c5e1c3fa3c1acbc@outlook.com>
2019-11-27 08:13:02 +0300

    newfork

--- fork-run.js
+++ fork-run.js
@@ -1,6 +1,6 @@
 global.FORK_MODE = 1; //set to 1 for fork
 global.NETWORK = "WBN-second"; //max 10 chars
-global.START_NETWORK_DATE = 1574793000000; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
+global.START_NETWORK_DATE = 1574831700000; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
 global.CONSENSUS_PERIOD_TIME = 2000; //ms
 global.FORK_IP_LIST = [
   {"ip":"193.124.180.20","port":7734},
